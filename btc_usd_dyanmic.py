ca6287b9ce8f4f4c4e7b68ddbbd3bf1c5c0f5b88
Yashasvi Baneswar Mishra <87185713+yashasvibmishra@users.noreply.github.com>
2022-03-11 10:30:15 +0530

    Update btc_usd_dyanmic.py

--- btc_usd_dyanmic.py
+++ btc_usd_dyanmic.py
@@ -1,8 +1,4 @@
-# Copyright (C) Yashasvi Baneswar Mishra - All Rights Reserved
-# Unauthorized copying of this file, via any medium is strictly prohibited
-# Proprietary and confidential
-# Written by Yashasvi Baneswar Mishra <yashasvimishra1@gmail.com>, March 2022
- 
+
 
 
 
