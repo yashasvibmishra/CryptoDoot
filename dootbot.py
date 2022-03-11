f9b374b9713d2d660077122fa3b5172b75040012
Yashasvi Baneswar Mishra <87185713+yashasvibmishra@users.noreply.github.com>
2022-03-11 13:39:53 +0530

    Update dootbot.py

--- dootbot.py
+++ dootbot.py
@@ -1,7 +1,4 @@
-# Copyright (C) Yashasvi Baneswar Mishra - All Rights Reserved
-# Unauthorized copying of this file, via any medium is strictly prohibited
-# Proprietary and confidential
-# Written by Yashasvi Baneswar Mishra <yashasvimishra1@gmail.com>, March 2022
+
 
 import telegram
 from telegram.ext import Updater
